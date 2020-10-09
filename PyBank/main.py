7711699b0ff11fb9f0debb270116faf3ee0038f2
abarnatan <ale.barn19@gmail.com>
2020-10-08 21:01:25 -0700

    adding P/L totals, reworked row counter

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,23 +9,29 @@ csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.DictReader(csvfile, delimiter=',')
+    csvreader = csv.reader(csvfile, delimiter=',')
 
 
     #skip header row
     next(csvreader, None)
 
     # Create analysis output
-    print("Financial Analysis")
-    print("-------------------------------")
+    #print("Financial Analysis")
+    #print("-------------------------------")
 
     # Count number of months included in dataset
-    num_of_rows = sum(1 for line in csvfile)
-    print("Number of months:" ,num_of_rows)
+    #num_of_rows = sum(1 for line in csvfile)
+    #print("Number of months:" ,num_of_rows)
 
     # Net total of profit/losses over period 
-    totals = []
-    for num_of_rows, row in enumerate(csvreader, start=1):
-        value = int(row['Profit/Losses'])
-        totals.append(value)
-    print("Total P/L:" ,totals)
+    month_count = 0
+    total_value = 0
+    for row in csvreader:
+        month_count = month_count + 1
+        total_value = total_value + int(row[1])
+   
+
+print("Financial Analysis")
+print("-------------------------------")
+print("Number of months:" ,month_count)
+print("Total P/L:" ,total_value)
