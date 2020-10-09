1a1a1059f2a370f1dc8eb5eb3c6125913581cdde
abarnatan <ale.barn19@gmail.com>
2020-10-08 18:50:22 -0700

    adjusted row counting script

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,21 +11,17 @@ with open(csvpath) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
+    #skip header row
+    next(csvreader, None)
+
     # Create analysis output
     print("Financial Analysis")
     print("-------------------------------")
+    
     # Count number of months included in dataset
-    num_of_rows = sum(1 for line in csvfile) - 1
-
+    num_of_rows = sum(1 for line in csvfile)
     print("Number of months:" ,num_of_rows)
 
+    # Net otal of profit/losses over period 
 
-
-    # print(csvreader)
-
-    # Read the header row first (skip this step if there is now header)
-    # csv_header = next(csvreader)
-    # print(f"CSV Header: {csv_header}")
-
-    # for row in csvreader:
-     #     print(row)
+    
