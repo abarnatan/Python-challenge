5a564addaf6f3d9b06b567035d7b00f5a1dca768
abarnatan <ale.barn19@gmail.com>
2020-10-08 19:07:53 -0700

    close to adding up total P/L

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,7 +9,8 @@ csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile, delimiter=',')
+    csvreader = csv.DictReader(csvfile, delimiter=',')
+
 
     #skip header row
     next(csvreader, None)
@@ -17,11 +18,14 @@ with open(csvpath) as csvfile:
     # Create analysis output
     print("Financial Analysis")
     print("-------------------------------")
-    
+
     # Count number of months included in dataset
     num_of_rows = sum(1 for line in csvfile)
     print("Number of months:" ,num_of_rows)
 
-    # Net otal of profit/losses over period 
-
-    
+    # Net total of profit/losses over period 
+    totals = []
+    for num_of_rows, row in enumerate(csvreader, start=1):
+        value = int(row['Profit/Losses'])
+        totals.append(value)
+    print("Total P/L:" ,totals)
