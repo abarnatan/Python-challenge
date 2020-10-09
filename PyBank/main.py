2dfe5a7ed93e91d85bd311a20ef635fda71662d8
abarnatan <ale.barn19@gmail.com>
2020-10-08 18:41:31 -0700

    script now outputting number of months in dataset :D

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,13 +11,21 @@ with open(csvpath) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    
+    # Create analysis output
+    print("Financial Analysis")
+    print("-------------------------------")
+    # Count number of months included in dataset
+    num_of_rows = sum(1 for line in csvfile) - 1
 
-    print(csvreader)
+    print("Number of months:" ,num_of_rows)
+
+
+
+    # print(csvreader)
 
     # Read the header row first (skip this step if there is now header)
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    # csv_header = next(csvreader)
+    # print(f"CSV Header: {csv_header}")
 
-    for row in csvreader:
-        print(row)
+    # for row in csvreader:
+     #     print(row)
