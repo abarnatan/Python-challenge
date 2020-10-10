d91a526644c1d5ed6edf3212aa96c6f49de160df
abarnatan <ale.barn19@gmail.com>
2020-10-10 14:57:35 -0700

    added new variables to calculate changes betweeen months

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,19 +14,40 @@ with open(csvpath) as csvfile:
 
     #skip header row
     next(csvreader, None)
-
+    first_row = next(csvreader)
+    print(first_row)
+    # Define Variables
+    month_count = 1
+    total_value = int(first_row[1]) 
+    old_month = int(first_row[1]) 
+    new_month = 0
+    PLchange = 0 
+    monthList = []
     # Create analysis output
-    
     # Count number of months included in dataset
     # Net total of profit/losses over period 
-    month_count = 0
-    total_value = 0
     for row in csvreader:
         month_count = month_count + 1
         total_value = total_value + int(row[1])
+        
+        
+        # Calculate Avg change in P/L
+        # store previous value
+        # store current value 
+        # subtract current - previous 
+        # loop through and average 
+        new_month = int(row[1]) 
+        PLchange = new_month - old_month
+        monthList.append(PLchange)
+        old_month = new_month
+
+# sum of month list / length of month list 
+print(monthList)
+
    
 
 print("Financial Analysis")
 print("-------------------------------")
 print("Number of months:" ,month_count)
 print("Total P/L:" ,total_value)
+print("Average change in P/L")
