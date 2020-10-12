43d67279d8280953e3166eb3340e5bbac6ba0d83
abarnatan <ale.barn19@gmail.com>
2020-10-11 23:38:19 -0700

    weird glitch in original script

---
+++ PyBank/PyBank.py
@@ -0,0 +1,52 @@
+# import modules 
+import os 
+import csv
+
+# Read file 'budget_data.csv'
+
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+
+    #skip header row
+    next(csvreader, None)
+    # first_row = next(csvreader)
+   
+    # Define Variables
+    month_count = []
+    total_value = []
+    PLchange = [] 
+   
+    
+    # Create analysis output
+    # Count number of months included in dataset
+    # Net total of profit/losses over period 
+    for row in csvreader:
+        month_count.append(row[0])
+
+        total_value.append(int(row[1])) 
+        
+    for x in range (1,len(total_value)):
+        PLchange.append(int(total_value[x])-int(total_value[x-1]))
+   
+    # print(PLchange)    
+
+    totalmonths = len(month_count)
+    totalProfit = sum(total_value)
+    AvgChange = sum(PLchange) / len(PLchange)
+    GreatestIncrease = max(PLchange)
+    GreatestDecrease = min(PLchange)
+    GreatestIncreaseDate = month_count[PLchange.index(GreatestIncrease)+1] 
+    GreatestDecreaseDate = month_count[PLchange.index(GreatestDecrease)+1]
+
+print(totalmonths,totalProfit,AvgChange,GreatestIncrease,GreatestDecrease,GreatestIncreaseDate,GreatestDecreaseDate)
+
+print("Financial Analysis")
+print("-------------------------------")
+print("Number of months:" + str(month_count))
+print("Total P/L:" + str(total_value)
+print(f"Average change in P/L: ${AvgChange}")
--- PyBank/main.py
+++ PyBank/main.py
@@ -15,39 +15,43 @@ with open(csvpath) as csvfile:
     #skip header row
     next(csvreader, None)
     first_row = next(csvreader)
-    print(first_row)
+   
     # Define Variables
-    month_count = 1
-    total_value = int(first_row[1]) 
+    month_count = []
+    total_value = []
     old_month = int(first_row[1]) 
     new_month = 0
-    PLchange = 0 
+    PLchange = [] 
     monthList = []
+    Sum_Month_List = 0
+    Len_Month_List = 0
     # Create analysis output
     # Count number of months included in dataset
     # Net total of profit/losses over period 
     for row in csvreader:
-        month_count = month_count + 1
-        total_value = total_value + int(row[1])
+        month_count.append(row[0])
+
+        total_value.append(int(row[1]) 
         
-        
+    for x in range (len(total_value)-1):
+        PLchange.append(total_value[x+1]- total_value[x])
+    
+    totalmonths = len(month_count)
+    totalProfit = sum(total_value)
+    AvgChange = sum(PLchange) / len(PLchange)
+    print(totalmonths,totalProfit,AvgChange)
         # Calculate Avg change in P/L
         # store previous value
         # store current value 
         # subtract current - previous 
         # loop through and average 
-        new_month = int(row[1]) 
-        PLchange = new_month - old_month
-        monthList.append(PLchange)
-        old_month = new_month
-
+       
 # sum of month list / length of month list 
-print(monthList)
 
    
 
-print("Financial Analysis")
-print("-------------------------------")
-print("Number of months:" ,month_count)
-print("Total P/L:" ,total_value)
-print("Average change in P/L")
+#print("Financial Analysis")
+#print("-------------------------------")
+# print("Number of months:" ,month_count)
+# print("Total P/L:" ,total_value)
+# print("Average change in P/L:" ,Avg_PL)
