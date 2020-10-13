6a5ca964387348f83f5f0cd3e7664eee44ad9859
abarnatan <ale.barn19@gmail.com>
2020-10-12 22:20:47 -0700

    script wrighitng to terminal correct data

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,44 +14,41 @@ with open(csvpath) as csvfile:
 
     #skip header row
     next(csvreader, None)
-    first_row = next(csvreader)
+    # first_row = next(csvreader)
    
     # Define Variables
     month_count = []
     total_value = []
-    old_month = int(first_row[1]) 
-    new_month = 0
     PLchange = [] 
-    monthList = []
-    Sum_Month_List = 0
-    Len_Month_List = 0
+   
+    
     # Create analysis output
     # Count number of months included in dataset
     # Net total of profit/losses over period 
     for row in csvreader:
         month_count.append(row[0])
 
-        total_value.append(int(row[1]) 
+        total_value.append(int(row[1])) 
         
-    for x in range (len(total_value)-1):
-        PLchange.append(total_value[x+1]- total_value[x])
-    
+    for x in range (1,len(total_value)):
+        PLchange.append(int(total_value[x])-int(total_value[x-1]))
+   
+    # print(PLchange)    
+
     totalmonths = len(month_count)
     totalProfit = sum(total_value)
     AvgChange = sum(PLchange) / len(PLchange)
-    print(totalmonths,totalProfit,AvgChange)
-        # Calculate Avg change in P/L
-        # store previous value
-        # store current value 
-        # subtract current - previous 
-        # loop through and average 
-       
-# sum of month list / length of month list 
+    GreatestIncrease = max(PLchange)
+    GreatestDecrease = min(PLchange)
+    GreatestIncreaseDate = month_count[PLchange.index(GreatestIncrease)+1] 
+    GreatestDecreaseDate = month_count[PLchange.index(GreatestDecrease)+1]
 
-   
 
-#print("Financial Analysis")
-#print("-------------------------------")
-# print("Number of months:" ,month_count)
-# print("Total P/L:" ,total_value)
-# print("Average change in P/L:" ,Avg_PL)
+
+print("Financial Analysis")
+print("-------------------------------")
+print("Number of months:" + str(totalmonths))
+print(f"Total: ${totalProfit}")
+print(f"Average Change: ${AvgChange}")
+print(f"Greates Increase in Profits: {GreatestIncreaseDate} ${GreatestIncrease}")
+print(f"Greates Decrease in Profits: {GreatestDecreaseDate} ${GreatestDecrease}")
